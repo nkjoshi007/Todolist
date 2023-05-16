c92419f9aaacdfc1b0163cb027865b823fefde95
naveensharma <sharmanaveen0010@gmail.com>
2023-05-16 18:31:45 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Todolist from './Todolist';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App/>
     <Todolist/>
   </React.StrictMode>
 );
