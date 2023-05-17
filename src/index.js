156131367807c1f8c68aeb6d4cee16b1c01e7a96
naveensharma <sharmanaveen0010@gmail.com>
2023-05-17 16:22:31 +0530

    made the todo list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Todolist from './Todolist';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {store} from './redux/store';
+import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
-    <Todolist/>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
